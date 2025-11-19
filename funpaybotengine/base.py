152b3788d015ce2c8dc254fd364738ca09f518b5
qvvonk <qvvonk@gmail.com>
2025-11-20 00:46:58 +0300

    deleted __init__ in BindableObject

--- funpaybotengine/base.py
+++ funpaybotengine/base.py
@@ -23,9 +23,6 @@ F = TypeVar('F', bound=Callable[..., Any])
 class BindableObject(BaseModel):
     _bot: Bot | None = PrivateAttr(None)
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
-
     def model_post_init(self, context: dict[Any, Any]) -> None:
         self._bot = context.get('bot') if context else None
 
