92bd682d40262a51271651df9fd80cb170f2e9c4
qvvonk <qvvonk@gmail.com>
2025-12-08 13:19:24 +0300

    Authentication check in EventCollector._get_chat_bookmarks()

--- funpaybotengine/runner/event_collector.py
+++ funpaybotengine/runner/event_collector.py
@@ -4,13 +4,14 @@ import time
 from typing import TYPE_CHECKING, Any, Type, Literal, TypeVar
 from collections.abc import Callable
 
+from funpaybotengine.exceptions import BotUnauthorizedError
 from funpaybotengine.utils import random_runner_tag
 from funpaybotengine.loggers import runner_logger as logger
 from funpaybotengine.dispatching import RunnerEvent
 from funpaybotengine.types.enums import MessageType, OrderPreviewType
 from funpaybotengine.runner.config import RunnerConfig
 from funpaybotengine.types.messages import Message
-from funpaybotengine.types.requests.runner import NodeRequestObject, ChatBookmarksRequestObject
+from funpaybotengine.types.requests.runner import NodeRequestObject, ChatBookmarksRequestObject, OrdersCountersRequestObject
 from funpaybotengine.storage.inmemory_storage import InMemoryStorage
 from funpaybotengine.exceptions.session_exceptions import UnexpectedHTTPStatusError
 from funpaybotengine.dispatching.events.builtin_events import (
@@ -175,7 +176,14 @@ class EventCollector:
     @attempts()
     async def _get_chat_bookmarks(self) -> RunnerResponse:
         async with self.bot._messages_lock:
-            return await self.bot.runner_request(objects_to_request=[ChatBookmarksRequestObject()])
+            result = await self.bot.runner_request(
+                objects_to_request=[
+                    ChatBookmarksRequestObject(),
+                    OrdersCountersRequestObject()
+                ])
+            if not result.orders_counters or not result.orders_counters.data:
+                raise BotUnauthorizedError()
+            return result
 
     @attempts()
     async def _get_sales(self, order_id: str | None = None) -> tuple[OrderPreview, ...]:
