c7c356f7c07ddeec46ca912b096ed3597a950ee5
Egor <qvvonk@gmail.com>
2026-03-23 21:15:30 +0000

    Revert "reexport GetReviews from methods (#30)"
    This reverts commit f9cd8748520d66641c0e99f393f1b2ecf14806a7.

--- funpaybotengine/methods/__init__.py
+++ funpaybotengine/methods/__init__.py
@@ -12,7 +12,6 @@ from .check_banned import *
 from .raise_offers import *
 from .upload_image import *
 from .delete_review import *
-from .get_reviews import *
 from .get_chat_page import *
 from .get_main_page import *
 from .get_purchases import *
